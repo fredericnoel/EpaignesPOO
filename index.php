e2336b86c7de807e5e61de87d06d22e987d5e3f5
Frédéric NOËL <contact@fredericnoel.com>
2019-03-06 12:43:50 +0100

    Ajout méthode statique logWrite()

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
+date_default_timezone_set('Europe/Paris');
 require_once "./functions/classAutoLoader.php";
 spl_autoload_register('classAutoLoader');
 
-$merlin = new Magicien();
+$merlin = new Magicien("Macron");
+
+Log::logWrite("Coucou");
 
 var_dump($merlin);
